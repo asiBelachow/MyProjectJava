7d47f4edd718002ea6e32cab5e28e46c5f4a1a65
asiBelachow <israelisraeli488@gmail.com>
2016-08-30 23:03:25 +0300

    Add description for classes and methods in package

--- my algorithms/src/io/MyCompressorOutputStream.java
+++ my algorithms/src/io/MyCompressorOutputStream.java
@@ -4,20 +4,44 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+
+
+/**
+ * The Class MyCompressorOutputStream save a Maze3D object to a output stream 
+ * 
+ * @author Asi Belachow
+ * @version 1.0
+ * @since 2016-28-08
+ */
 public class MyCompressorOutputStream  extends OutputStream{
 
 	
+
 	private OutputStream out;
 	
+	//------------------------------Constructors-------------------------//
+	
+	/**
+	 * <h1>MyCompressorOutputStream</h1><p>
+	 * <i><ul>MyCompressorOutputStream(OutputStream out)<i><p>
+	 * Initialize a new compressor
+	 * @param out - OutputStream 
+	 */
 	public MyCompressorOutputStream(OutputStream out) {
 		this.out = out;
+		
 	}
+	
+	//-------------------------Functionality-------------------------//
+	
 	
 	@Override
 	public void write(int b) throws IOException {
 		
 	}
 	
+	
+	@Override
 	public void write(byte[] array) throws IOException{
 		DataOutputStream data = new DataOutputStream(out);
 		int counter = 0;
--- my algorithms/src/io/MyDecompressorInputStream.java
+++ my algorithms/src/io/MyDecompressorInputStream.java
@@ -1,13 +1,33 @@
 package io;
 
 import java.io.DataInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
 
+
+
+/**
+ * The Class MyDecompressorInputStream read a Maze3D object from a input stream 
+ * 
+ * @author Asi Belachow
+ * @version 1.0
+ * @since 2016-28-08
+ */
 public class MyDecompressorInputStream extends InputStream {
 
 	private InputStream in;
 	
+	
+	
+	//------------------------------Constructors-------------------------//
+	
+	/**
+	 * <h1>MyDecompressorInputStream</h1><p>
+	 * <i><ul>MyDecompressorInputStream(InputStream in)<i><p>
+	 * Initialize a new Decompressor
+	 * @param out - InputStream
+	 */
 	public MyDecompressorInputStream(InputStream in) {
 		this.in = in;
 	}
@@ -18,7 +38,7 @@ public class MyDecompressorInputStream extends InputStream {
 		return 0;
 	}
 	
-	
+	@Override
 	public int read(byte[] array) throws IOException{
 		
 		DataInputStream data = new DataInputStream(in);
